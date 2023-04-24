2c3eacb7e9c3dc7a753331281dcb4bc433e6f43b
Thiago Lira <thiago.slira@outlook.com>
2023-04-24 17:02:45 -0300

    Criado arquivos com setup inicial para rota de /login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import productsRouter from './products.routes';
 import usersRouter from './users.routes';
 import ordersRouter from './orders.routes';
+import loginRouter from './login.routes';
 
 const router = Router();
 
 router.use('/products', productsRouter);
 router.use('/users', usersRouter);
 router.use('/orders', ordersRouter);
+router.use('/login', loginRouter);
 
 export default router;
---
+++ src/services/login.service.ts
@@ -0,0 +1,7 @@
+async function userLogin() {
+  
+}
+
+export default {
+  userLogin,
+};
