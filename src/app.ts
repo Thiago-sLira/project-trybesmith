642f12e542628b3f28a88d26e7b36555c88a022d
Thiago Lira <thiago.slira@outlook.com>
2023-04-24 15:02:32 -0300

    Middleware de tratamento de erros adicionado no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import router from './routes';
+import errorHandler from './middlewares/errorHandler';
+import 'express-async-errors';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(router);
+
+app.use(errorHandler);
 
 export default app;
