65ab33762ca4fe40907916dd0b5afbf32d09780e
Thiago Lira <thiago.slira@outlook.com>
2023-04-24 15:39:29 -0300

    Configurado arquivos para a rota de /users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import productsRouter from './products.router';
+import productsRouter from './products.routes';
+import usersRouter from './users.routes';
 
 const router = Router();
 
 router.use('/products', productsRouter);
+router.use('/users', usersRouter);
 
 export default router;
